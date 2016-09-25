3b01d2f2cfecb6a95e39df90bb2b86c29087b8ba
Mark <mark@remarkablemark.org>
2016-09-25 15:25:26 -0400

    Enable hot reloading with webpack-dev-server
    - Save dependency:
      * babel-preset-react-hmre@1.1.1
    - Update npm script:
      * dev: enable `hot` and `inline` options
    
    http://matthewlehner.net/react-hot-module-replacement-with-webpack/#enabling-hot-module-replacement-hmr

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'react-hmre', 'es2015']
                 }
             }
         ]
