22584abacd6068fd00ac5e5e2f33ef1276bd2e3e
Mark <mark@remarkablemark.org>
2016-09-25 15:29:15 -0400

    Refactor component in `main.js` to `App.jsx`
    This will allow us to see React hot reloading in action without
    a hard refresh.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App.jsx';
 
 render(
-    <h1>Welcome to React Quickstart</h1>,
+    <App />,
     document.getElementById('root')
 );
