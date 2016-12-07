f08a3dd6492ff8e32b91e967b99107ce5184ab62
Mark <mark@remarkablemark.org>
2016-12-07 16:05:59 -0500

    Update regex in `webpack.config.js`

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /.jsx?$/,
+                test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
